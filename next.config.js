be8ec58b4aa807558035dbe0b888aaff92755b88
Francesco Fontana <francesco.fontana@example.com>
2025-03-16 22:38:38 +0100

    fix: simplify deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,28 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, must-revalidate',
-          },
-        ],
-      },
-    ];
-  },
   poweredByHeader: false,
-  generateEtags: false,
-  compress: true,
-  distDir: '.next',
   typescript: {
     ignoreBuildErrors: true,
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+  }
 }
 
 module.exports = nextConfig 
